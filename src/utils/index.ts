e1e9d42be7c8276969520ff1dceffc691fedda42
Aliaksei Drabushau <a.drabushev@geozilla.com>
2024-11-13 16:13:30 +0000

    updated parser

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,13 @@
+const safeDecode = (val: string) => {
+  try {
+    return decodeURIComponent(val);
+  } catch (e) {
+    console.error(e);
+    console.warn('Failed to decodeURIComponent. This string isn\'t valid: ', val);
+    return val;
+  }
+};
+
 export const queryParser = (str: string): {[key: string]: string} => {
   const cutStr = str.slice(1);
   const coupleStr = cutStr.split('&').map(item => item.split('='));
