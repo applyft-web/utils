b3aea258f30eb833df7f54ac1dc2464f41f1ee13
Aliaksei Drabushau <aliakseidrabushau@gmail.com>
2025-06-04 14:19:32 +0100

    fix: updated release.config.js

--- release.config.js
+++ release.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   branches: [
-    { name: 'dev',  channel: 'dev',  prerelease: 'dev', tagFormat: 'v${version}-dev.${prerelease}'  },
-    { name: 'stage', channel: 'stage', prerelease: 'stage', tagFormat: 'v${version}-stage.${prerelease}' },
+    { name: 'dev',  channel: 'dev',  prerelease: 'dev', tagFormat: false  },
+    { name: 'stage', channel: 'stage', prerelease: 'stage', tagFormat: false },
     { name: 'main' }
   ],
   repositoryUrl: 'https://github.com/applyft-web/utils.git',
