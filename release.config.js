d7504b03e0a6c7113e8d57342bc002bb7487d563
Aliaksei Drabushau <aliakseidrabushau@gmail.com>
2025-06-04 14:23:17 +0100

    fix: updated release.config.js

--- release.config.js
+++ release.config.js
@@ -2,20 +2,12 @@ module.exports = {
   branches: [
     { name: 'dev',  channel: 'dev',  prerelease: 'dev', tagFormat: false  },
     { name: 'stage', channel: 'stage', prerelease: 'stage', tagFormat: false },
-    { name: 'main' }
+    { name: 'main', tagFormat: false }
   ],
   repositoryUrl: 'https://github.com/applyft-web/utils.git',
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     '@semantic-release/npm',
-    '@semantic-release/github',
-    [
-      '@semantic-release/git',
-      {
-        assets: ['package.json', 'CHANGELOG.md'],
-        message: 'chore(release): ${nextRelease.version} [skip ci]'
-      }
-    ]
   ]
 }
