20fda512b56671c778b6fcc9784e5d5fc0653c9d
Aliaksei Drabushau <aliakseidrabushau@gmail.com>
2025-07-24 14:32:35 +0100

    fix: release version

--- release.config.js
+++ release.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   branches: [
-    { name: 'dev',  channel: 'dev',  prerelease: 'dev', tagFormat: false  },
+    { name: 'dev', channel: 'dev', prerelease: 'dev', tagFormat: false },
     { name: 'stage', channel: 'stage', prerelease: 'stage', tagFormat: false },
     { name: 'main' }
   ],
@@ -9,5 +9,9 @@ module.exports = {
     '@semantic-release/commit-analyzer',
     // '@semantic-release/release-notes-generator',
     '@semantic-release/npm',
+    ['@semantic-release/git', {
+      assets: ['package.json', 'CHANGELOG.md'],
+      message: `chore(release): ${'${'}nextRelease.version${'}'} [skip ci]`
+    }]
   ]
 }
