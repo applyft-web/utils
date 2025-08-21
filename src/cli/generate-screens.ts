16f7eb9bfcbc9b30645a30bf862dad3ec5c0a2fd
Aliaksei Drabushau <aliakseidrabushau@gmail.com>
2025-08-21 18:42:24 +0100

    fix: update generateScreens to exclude certain keys based on configuration

--- src/cli/generate-screens.ts
+++ src/cli/generate-screens.ts
@@ -6,11 +6,24 @@ export function generateScreens (configRelPath: string, outRelPath: string): voi
   const outPath = path.resolve(process.cwd(), outRelPath)
 
   const { pagesConfig } = require(configPath) as { pagesConfig: Record<string, any> }
-  const screens = Object.keys(pagesConfig).filter(key => (key !== '*' || pagesConfig[key].excludeFromBuilder))
+  const { screens, excludedScreens } = Object.entries(pagesConfig).reduce(
+    (acc, [key, conf]) => {
+      if (key !== '*' && !conf.excludeFromBuilder) {
+        acc.screens.push(key)
+      } else {
+        acc.excludedScreens.push(key)
+      }
+      return acc
+    },
+    { screens: [], excludedScreens: [] }
+  )
 
   try {
     fs.writeFileSync(outPath, JSON.stringify(screens, null, 2), 'utf-8')
     console.log(`✅ screens.json was generated (${screens.length} screen${screens.length > 1 ? 's' : ''}): ${outPath}`)
+    if (excludedScreens?.length > 0) {
+      console.log(`Excluded screens (${excludedScreens.length}): ${excludedScreens.join(', ')}`)
+    }
   } catch (err) {
     console.error('❌ Failed to generate screens.json:', err)
     process.exit(1)
