69eed9924aeb70ffb01d5df46b29ce2c9ef4fff7
Aliaksei Drabushau <aliakseidrabushau@gmail.com>
2025-11-11 13:06:40 +0000

    fix: update useFlow to separate hooks import and clean up formatting

--- src/flows/index.ts
+++ src/flows/index.ts
@@ -1,19 +1,20 @@
-import { useState, useEffect } from 'react';
-import { useConf, printLogs } from '../utils';
+import { useState, useEffect } from 'react'
+import { printLogs } from '../utils'
+import { useConf } from '../hooks'
 
 export const useFlow = (flowType: string, flowsList: Record<string, string[]>) => {
-  const [flow, setFlow] = useState<any>();
-  const conf = useConf('flows');
+  const [flow, setFlow] = useState<any>()
+  const conf = useConf('flows')
 
   useEffect(() => {
-    if (!flowType || conf === undefined) return;
-    const localFlow = flowsList[flowType];
-    const customFlow = conf?.[flowType];
-    setFlow(customFlow ?? localFlow ?? null);
+    if (!flowType || conf === undefined) return
+    const localFlow = flowsList[flowType]
+    const customFlow = conf?.[flowType]
+    setFlow(customFlow ?? localFlow ?? null)
     if (!customFlow && !localFlow) {
-      printLogs(`result flow «${flowType}» does not exist. set default default flow`);
+      printLogs(`result flow «${flowType}» does not exist. set default default flow`)
     }
-  }, [conf, flowType, flowsList]);
+  }, [conf, flowType, flowsList])
 
-  return flow;
-};
+  return flow
+}
